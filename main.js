8c530f8dcc441101b60b7f42837709656e655c86
Emily DeWitt <emilykdewitt@gmail.com>
2019-03-19 18:17:31 -0500

    ain.js

--- main.js
+++ main.js
@@ -3,27 +3,41 @@ const printToDom = (divId, textToPrint) => {
     selectedDiv.innerHTML = textToPrint;
   };
 
+const toCelsius = (temp) => {
+    const newTemp = 
+}
+const toFahrenheit = (temp) => {
+
+}
+
 const determineConverter = () => {
     let domString = ``;
+    const temp = document.getElementById('tempInput').value;
     if (document.getElementById('celsius').checked) {
         tempType = document.getElementById('celsius').value;
+        const convertedTemp = toFahrenheit(temp);
       }
     else if (document.getElementById('fahrenheit').checked) {
         tempType = document.getElementById('fahrenheit').value;
+        const convertedTemp = toCelsius(temp);
     }
-    const tempValue = document.getElementById('tempInput').value;
-    console.log(document.getElementById('tempInput').value);
+    console.log(temp);
     console.log(tempType);
     domString += `<div class="tempOutputBox">`
     domString +=    `<h5>${tempValue}</h5>`;
     domString +=    `<h5>${tempType}</h5>`;
-    domString += `</div>`
+    domString += `</div>`;
     printToDom('tempOutput', domString);
  };
 
- /*determineConverter function should console.log value in input and should console.log either C or F depending on which radio button is selected, print the tempInput value to the output div
-Write a printToDom function - 2 inputs - divId and textToPrint
-*/
+
+determineConverter should call either toCelsius or toFahrenheit depending on what radio button is selected
+toCelsius and toFahrenheit should each accept 1 input - temp which is a number
+toCelsius and toFahrenheit should convert the temperature
+toCelsius and toFahrenheit should call domStringBuilder function and pass final number and C or F
+domStringBuilder should have two inputs - finalTemp and unit
+domStringBuilder should build a h2 tag that looks something like 27 degrees F
+domStringBuilder should call printToDom and pass the id of tempOutput and h2 string created
 
 const init = () => {
      determineConverter();
